198b31f941061304b1dfab4c34a9308747c81f24
G4bbix <56590039+G4bbix@users.noreply.github.com>
2024-05-03 09:58:16 +0200

    First usable version

---
+++ lua/cherry/init.lua
@@ -0,0 +1,147 @@
+local M = {}
+
+vim.g.cherry_pairs = {
+	default = {
+		{ "{", "}" },
+		{ "[", "]" },
+		{ "(", ")" },
+	},
+	vim = {
+		{ "if", "endif" },
+		{ "function", "endfunction" },
+		{ "while", "endwhile" },
+		{ "for", "endfor" },
+		{ "try", "endtry" },
+	},
+	sh = {
+		{ "if", "fi" },
+		{ "for", "done" },
+		{ "while", "done" },
+		{ "case", "esac" },
+	},
+	lua = {
+		{ "function", "end" },
+		{ "if", "end" },
+		{ "do", "end" },
+		{ "repeat", "until" },
+	},
+}
+
+M.setup = function()
+	vim.cmd("redir >> debug")
+	vim.t.current_cherry_pairs = vim.g.cherry_pairs["default"]
+	if vim.g.cherry_pairs[vim.bo.filetype] == nil then
+		return
+	end
+
+	local temp_table = vim.t.current_cherry_pairs
+	for _, pair in pairs(vim.g.cherry_pairs[vim.bo.filetype]) do
+		if vim.g.cherry_pairs[pair[1]] ~= nil then
+			error("CHERRY ERROR: Opening word " .. pair[1] .. " already exists")
+		end
+		table.insert(temp_table, pair)
+	end
+	vim.t.current_cherry_pairs = temp_table
+
+	for i, list in ipairs(vim.g.cherry_highlights) do
+		local hi_cmd = "highlight Cherry-" .. i
+		for key, val in pairs(list) do
+			hi_cmd = hi_cmd .. " " .. key .. "=" .. val
+		end
+		vim.cmd(hi_cmd)
+	end
+end
+
+local highlights = {}
+local function highlight()
+	for i, result in pairs(vim.t.cherry_results) do
+		local match_id = vim.fn.matchaddpos("Cherry-" .. i, result)
+		highlights[tostring(match_id)] = result
+	end
+end
+
+-- @param[range] range of lines to be cleared {1, 3}
+local function clear_matches(range)
+	if highlights == nil then
+		return
+	end
+
+	for id, val in pairs(highlights) do
+		local delete_match = true
+		if range ~= nil then
+			if val[1] < range[1] and val[2] > range[2] then
+				delete_match = false
+			end
+		end
+		if delete_match then
+			highlights[id] = nil
+			vim.fn.matchdelete(tonumber(id))
+		end
+	end
+end
+
+function M.update_pairs()
+	print(123)
+	clear_matches()
+	vim.t.cherry_results = {}
+	local start_line = vim.api.nvim_call_function("line", { "w0" })
+	local last_line = vim.api.nvim_call_function("line", { "w$" })
+	vim.g.cherry_current_pos = vim.api.nvim_win_get_cursor(0)
+
+	vim.g.cherry_results = {}
+	for _, pair in pairs(vim.t.current_cherry_pairs) do
+		local open = pair[1]
+		local close = pair[2]
+		vim.api.nvim_call_function("searchpair", {
+			"\\m" .. open,
+			"",
+			"noop",
+			"rcnbW", -- r:outest pair; c:match oncursor; n:do not move cursor; b:search backwards; W:don't  wrap around
+			"getline('.') || g:Cherry_find_closing('" .. open .. "','" .. close .. "','" .. (last_line + 2) .. "')",
+			start_line,
+			30,
+		})
+	end
+	highlight()
+	vim.cmd("redir END")
+end
+
+function M.cherry_validate_ts(start_pos_1, end_pos_1, bufnr)
+	local start_pos = { start_pos_1[1] - 1, start_pos_1[2] - 1 }
+	local end_pos = { end_pos_1[1] - 1, end_pos_1[2] - 1 }
+	local start_node = vim.treesitter.get_node({ bufnr, pos = { start_pos[1], start_pos[2] } })
+	local end_node = vim.treesitter.get_node({ bufnr, pos = { end_pos[1], end_pos[2] } })
+	if start_node:id() == end_node:id() then
+		return 0
+	else
+		return 1
+	end
+end
+
+function M.cherry_aggregate_results(open, close, len_open, len_close)
+	local open_str = { tostring(open[1]), tostring(open[2]) }
+	local temp_table = vim.t.cherry_results
+	local target_index = 1
+
+	if table.getn(temp_table) ~= 0 then
+		for i = 1, table.getn(temp_table), 1 do
+			if
+				tonumber(temp_table[i][1][1]) > open[1]
+				or (tonumber(temp_table[i][1][1]) == open[1] and tonumber(temp_table[i][1][2]) > open[2])
+			then
+				target_index = i
+				break
+			elseif i == table.getn(temp_table) then
+				target_index = i + 1
+				break
+			end
+		end
+	end
+	table.insert(temp_table, target_index, {
+		{ tonumber(open_str[1]), tonumber(open_str[2]), len_open },
+		{ close[1], close[2], len_close },
+	})
+	vim.t.cherry_results = temp_table
+end
+
+return M
--- plugin/cherry.vim
+++ plugin/cherry.vim
@@ -1,1 +1,24 @@
-echomsg  "test2"
+function g:Cherry_find_closing(opening_str, closing_str, last_line)
+	let s:open = [line('.'), col('.')]
+	let s:current_buffer = bufnr('%')
+	let s:close = searchpairpos('\V' . a:opening_str, '', '\V' . a:closing_str, 'W',
+		\ "v:lua.require'cherry'.cherry_validate_ts(s:open, [line('.'), col('.')], s:current_buffer)",
+		\ a:last_line, 500)
+
+	if s:verify_scope()
+		call v:lua.require'cherry'.cherry_aggregate_results(s:open, s:close, len(a:opening_str), len(a:closing_str))
+	endif
+	return 0
+endfunction
+
+function s:verify_scope()
+	if s:close[0] > g:cherry_current_pos[0]
+		return 1
+	elseif s:close[0] == g:cherry_current_pos[0] &&
+		\ s:close[1] >= g:cherry_current_pos[1]
+		return 1
+	else
+		return 0
+	endif
+endfunction
+
