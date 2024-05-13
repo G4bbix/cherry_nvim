818b6e146a46ce1de77d510f13b1ff145cae0c5d
G4bbix <56590039+G4bbix@users.noreply.github.com>
2024-05-13 09:19:19 +0200

    Better setup and bugfixes for different tabs

--- lua/cherry/init.lua
+++ lua/cherry/init.lua
@@ -27,21 +27,7 @@ vim.g.cherry_pairs = {
 	},
 }
 
-M.setup = function()
-	vim.t.current_cherry_pairs = vim.g.cherry_pairs["default"]
-	if vim.g.cherry_pairs[vim.bo.filetype] == nil then
-		return
-	end
-
-	local temp_table = vim.t.current_cherry_pairs
-	for _, pair in pairs(vim.g.cherry_pairs[vim.bo.filetype]) do
-		if vim.g.cherry_pairs[pair[1]] ~= nil then
-			error("CHERRY ERROR: Opening word " .. pair[1] .. " already exists")
-		end
-		table.insert(temp_table, pair)
-	end
-	vim.t.current_cherry_pairs = temp_table
-
+local function setup()
 	for i, list in ipairs(vim.g.cherry_highlights) do
 		local hi_cmd = "highlight Cherry-" .. i
 		for key, val in pairs(list) do
@@ -49,23 +35,41 @@ M.setup = function()
 		end
 		vim.cmd(hi_cmd)
 	end
+
+	vim.t.current_cherry_pairs = vim.g.cherry_pairs["default"]
+	if vim.g.cherry_pairs[vim.bo.filetype] == nil then
+		return
+	end
+	local temp_table = vim.t.current_cherry_pairs
+	for _, pair in pairs(vim.g.cherry_pairs[vim.bo.filetype]) do
+		if temp_table[pair[1]] ~= nil then
+			error("CHERRY ERROR: Opening word " .. pair[1] .. " already exists")
+		end
+		table.insert(temp_table, pair)
+	end
+	vim.t.current_cherry_pairs = temp_table
 end
 
-local highlights = {}
 local function highlight()
+	local temp_table = vim.t.highlights
+	if temp_table == nil then
+		return
+	end
 	for i, result in pairs(vim.t.cherry_results) do
 		local match_id = vim.fn.matchaddpos("Cherry-" .. i, result)
-		highlights[tostring(match_id)] = result
+		temp_table[tostring(match_id)] = result
 	end
+	vim.t.highlights = temp_table
 end
 
 -- @param[range] range of lines to be cleared {1, 3}
 local function clear_matches(range)
-	if highlights == nil then
+	if vim.t.highlights == nil then
 		return
 	end
 
-	for id, val in pairs(highlights) do
+	local temp_table = vim.t.highlights
+	for id, val in pairs(vim.t.highlights) do
 		local delete_match = true
 		if range ~= nil then
 			if val[1] < range[1] and val[2] > range[2] then
@@ -73,15 +77,22 @@ local function clear_matches(range)
 			end
 		end
 		if delete_match then
-			highlights[id] = nil
+			temp_table[id] = nil
 			vim.fn.matchdelete(tonumber(id))
 		end
 	end
+	vim.t.highlights = temp_table
 end
 
 function M.update_pairs()
 	if vim.treesitter.language.get_lang(vim.bo.filetype) == nil then
 		return
+	end
+
+	if vim.t.cherry_setup == nil then
+		setup()
+		vim.t.highlights = {}
+		vim.t.cherry_setup = 1
 	end
 
 	clear_matches()
